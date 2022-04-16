2aafda7439327a7ddf2d5582d0dc1093e9b2660e
Ahringuci <biihuy25697@gmail.com>
2022-04-16 18:46:44 +0700

    update

--- gymcode/src/App.js
+++ gymcode/src/App.js
@@ -35,25 +35,29 @@ function App() {
     ];
     return (
         <div>
-            <h2>Students</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Company</th>
-                        <th>Contact</th>
-                        <th>Country</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {students.map((student) => (
-                        <tr>
-                            <td>{student.company}</td>
-                            <td>{student.contact}</td>
-                            <td>{student.country}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <h2>
+                [Bài tập] Tạo Element thể hiện Profile Card (như hình minh hoạ)
+            </h2>
+            <div className="container">
+                <div className="card">
+                    <div className="card--header" />
+                    <img
+                        className="avatar"
+                        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        alt="avatar"
+                    />
+                    <div className="card--body">
+                        <div>
+                            <p className="text-header">Ruma Khan</p>
+                            <p className="text-sub">
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry
+                            </p>
+                            <button className="btn third">FOLLOW</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
