c3fb070557d07b69b62889d58064d701c26552f4
Ahringuci <biihuy25697@gmail.com>
2022-04-16 19:10:02 +0700

    update

--- gymcode/src/App.js
+++ gymcode/src/App.js
@@ -1,85 +1,15 @@
 import "./App.css";
+import Alert from "./components/alert";
 
 function App() {
-    const students = [
-        {
-            company: "Alfreds Futterkiste",
-            contact: "Maria Anders",
-            country: "Germany",
-        },
-        {
-            company: "Centro comercial Moctezuma",
-            contact: "Francisco Chang",
-            country: "Mexico",
-        },
-        {
-            company: "Ernst Handel",
-            contact: "Roland Mendel",
-            country: "Austria",
-        },
-        {
-            company: "Island Trading",
-            contact: "Helen Bennett",
-            country: "UK",
-        },
-        {
-            company: "Laughing Bacchus Winecellars",
-            contact: "Yoshi Tannamuri",
-            country: "Canada",
-        },
-        {
-            company: "Magazzini Alimentari Riuniti",
-            contact: "Giovanni Rovelli",
-            country: "Italy",
-        },
-    ];
     return (
         <div>
-            <h2>
-                [Bài tập] Tạo Element thể hiện Profile Card (như hình minh hoạ)
-            </h2>
-            <div className="form-signin">
-                <form>
-                    <img
-                        className="mb-4"
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png"
-                        alt=""
-                        width="72"
-                        height="57"
-                    />
-                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-                    <div className="form-floating">
-                        <input
-                            type="email"
-                            className="form-control email"
-                            id="floatingInput"
-                            placeholder="name@example.com"
-                        />
-                        <label>Email address</label>
-                    </div>
-                    <div className="form-floating">
-                        <input
-                            type="password"
-                            className="form-control password"
-                            id="floatingPassword"
-                            placeholder="Password"
-                        />
-                        <label>Password</label>
-                    </div>
-                    <div className="checkbox mb-3">
-                        <label>
-                            <input type="checkbox" /> Remember me
-                        </label>
-                    </div>
-                    <button
-                        className="w-100 btn btn-lg btn-primary"
-                        type="submit"
-                    >
-                        Sign in
-                    </button>
-                    <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
-                </form>
-            </div>
+            <h2>[Bài tập] Xây dựng Alert component</h2>
+            <Alert
+                alertTxt={
+                    " Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại."
+                }
+            />
         </div>
     );
 }
