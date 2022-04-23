f0e6dcece0526a9e50c1d990202e368862587654
Ahringuci <biihuy25697@gmail.com>
2022-04-23 18:17:43 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "./components/title";
+import Homework from "./components/homework";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Title title="Xây dựng Component Example" />
+            <Homework />
+        </div>
+    );
+};
+
+export default App;
